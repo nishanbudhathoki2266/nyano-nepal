8024214e4150f67b9980b200cf36a361ad969881
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2024-06-20 20:34:23 +0545

    feat(initialization): basic clean up and initialization

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <h2 className="flex min-h-screen justify-center items-center text-5xl font-extrabold">
+      Warm Nepal
+    </h2>
+  );
+};
+
+export default Home;
